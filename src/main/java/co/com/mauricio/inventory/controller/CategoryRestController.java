32e306323740e7a11be580da7efb4daaa37bfabe
maurocastro29 <mm.castro.29@mail.com>
2023-10-03 15:30:17 -0500

    Configuracion del CrossOrigin para el el proyecto Frontend

--- src/main/java/co/com/mauricio/inventory/controller/CategoryRestController.java
+++ src/main/java/co/com/mauricio/inventory/controller/CategoryRestController.java
@@ -2,6 +2,7 @@ package co.com.mauricio.inventory.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -15,6 +16,8 @@ import co.com.mauricio.inventory.model.Category;
 import co.com.mauricio.inventory.response.CategoryResponseRest;
 import co.com.mauricio.inventory.services.ICategoryService;
 
+
+@CrossOrigin(origins = {"http://localhost:4200"})
 @RestController
 @RequestMapping("/api/v1")
 public class CategoryRestController {
